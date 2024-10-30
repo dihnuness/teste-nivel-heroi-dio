ace7f996f5d8680b06d758288ce47ca617caf1e5
dihnuness <dougsos11@gmail.com>
2024-10-29 21:44:16 -0300

    projeto criado

---
+++ src/index.js
@@ -0,0 +1,28 @@
+let nomeHeroi = "Kratos"
+let expHero = 10001
+
+if (expHero <= 1000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ferro")
+
+}else if (expHero >= 1001 && expHero <= 2000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Bronze")
+
+}else if (expHero >= 2001 && expHero <= 5000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Prata")
+
+}else if (expHero >= 5001 && expHero < 7000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ouro")
+
+}else if (expHero >= 7001 && expHero < 8000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Platina")
+
+}else if (expHero >= 8001 && expHero < 9000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ascendente")
+
+}else if (expHero >= 9001 && expHero < 10000){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Imortal")
+
+}else if (expHero >= 10001){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Radiante")
+
+}
